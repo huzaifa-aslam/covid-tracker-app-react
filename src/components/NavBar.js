dceb4003130a0d611ee516a7e034ea24eb0447af
Huzaifa Aslam <56935684+huzaifa-aslam@users.noreply.github.com>
2021-01-31 22:29:20 -0800

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -79,7 +79,7 @@ export default function NavBar() {
             <MenuIcon />
           </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
-            Covid Tracker App
+            Covid Tracker App 2020
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
